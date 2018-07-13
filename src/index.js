252a10a8eef3c2c8ad87d8d270983de5cc73d12f
Connor Dowson <connormwdowson@gmail.com>
2018-07-13 16:10:44 +0100

    Changed router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 
 import "./sass/main.scss";
 
 import Home from "./pages/Home"
+import PlayBilliards from "./pages/PlayBilliards"
 
 
 ReactDOM.render(
 
-    <Home text="hello"/>,
+    <HashRouter>
+
+        <div>
+
+            <Route exact path="/" component={PlayBilliards} />
+            <Route exact path="/" component={Home} />
+
+        </div>
+                
+    </HashRouter>,
     
     document.getElementById('app')
 
