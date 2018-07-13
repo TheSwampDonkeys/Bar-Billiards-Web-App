3367236388e33b01bb5305278074964ae21c17e2
finHards <finlayhards1@gmail.com>
2018-07-13 14:40:31 +0100

    setting react + babel up

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,33 @@
+const path = require('path');
+
 module.exports = options => {
+
     return {
-      entry: './index.js',
+
+      entry: './src/index.js',
       output: {
+
         filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+
       },
+
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              
+              loader: 'babel-loader',
+              options: {
+                presets: ['env', 'react']
+              }
+            }
+          }
+        ]
+      }
+
     }
+
   }
